692eed1dfbde0f0893feaec1ba46dd766b7435fe
AktugChelekche <aktugcilekci@hotmail.com>
2022-06-16 20:40:50 -0500

    Comparing Model

--- query.sql
+++
@@ -1,8 +0,0 @@
-SELECT S.artist, S.song, S.duration_ms, S.explicit, S.year , S.popularity ,S.danceability,
-S.energy,S.key,S.loudness,SS.mode,SS.speechiness,SS.acousticness,SS.instrumentalness ,
-SS.liveness,SS.valence,SS.tempo,SS.genre 
-into songs_normalize
-from songs1 S
-INNER JOIN songs2 SS
-ON S.song= SS.song 
-	
